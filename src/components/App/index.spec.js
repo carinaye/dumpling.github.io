41ae59808b48381ad1078f956afd2b815d10899e
carinaye <carinaye0401@gmail.com>
2023-08-15 02:18:52 +0800

    fixed unit test

--- src/components/App/index.spec.js
+++ src/components/App/index.spec.js
@@ -2,7 +2,6 @@ import {render, screen} from '@testing-library/react';
 
 import App from './App.js';
 
-Run | Debug
 test ('Render App',() => {
   render (<App />)
   const element = screen.getByText('Order')
